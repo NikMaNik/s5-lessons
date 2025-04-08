3bb77a597d72e732de14003ff4796dab3e6db605
NikMaNik <nikolay.m96@mail.ru>
2025-04-08 22:42:58 +0500

    add

--- Тема 3. Проектирование DWH/7. Хранение исторических данных/Задание 2/realization.sql
+++ Тема 3. Проектирование DWH/7. Хранение исторических данных/Задание 2/realization.sql
@@ -1,11 +1,11 @@
 -- Удалите внешний ключ из sales
-alter table public.sales drop constraint sales_clients_client_id_fk;
+alter table public.sales drop constraint sales_products_product_id_fk;
 
 -- Удалите первичный ключ из products
 alter table public.products drop constraint products_pk;
 
 -- Добавьте новое поле id для суррогантного ключа в products
-alter table pubic.products add column id int ;
+alter table public.products add column id serial ;
 
 -- Сделайте данное поле первичным ключом
 alter table public.products add primary key (id); 
@@ -17,4 +17,4 @@ alter table public.products add column valid_from timestamptz;
 alter table public.products add column valid_to timestamptz;
 
 -- Добавьте новый внешний ключ sales_products_id_fk в sales
-alter table public.sales add constraint sales_products_id_fk Foreign key (product_id) references public.product_id (id);
+alter table public.sales add constraint sales_products_id_fk Foreign key (product_id) references public.products (id);
