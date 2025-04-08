67e61e9eebc89548751aedddf2ed99e5e6f20a16
NikMaNik <nikolay.m96@mail.ru>
2025-04-09 03:13:49 +0500

    add

--- Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
+++ Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
@@ -7,8 +7,8 @@ SELECT
     , fps.total_sum as orders_total_sum
     , fps.bonus_payment as orders_bonus_payment_sum
     , fps.bonus_grant as orders_bonus_granted_sum
-    , fps.orders_total_sum * 0.25 as order_processing_fee
-    , fps.count - fps.orders_total_sum * 0.25 - fps.bonus_payment as restaurant_reward_sum
+    , fps.total_sum * 0.25 as order_processing_fee
+    , fps.count - fps.total_sum * 0.25 - fps.bonus_payment as restaurant_reward_sum
 FROM dds.fct_product_sales as fps
 JOIN dds.dm_orders as "do" on fps.order_id = "do".id 
 JOIN dds.dm_products as "dp" on fps.product_id = "dp".id
