0422e15d5d9a363eb5dce3991e9a6d82b5a3a719
NikMaNik <nikolay.m96@mail.ru>
2025-04-09 03:31:37 +0500

    add

--- Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
+++ Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
@@ -4,14 +4,15 @@ SELECT
     , "do".restaurant_id
     , "dr".restaurant_name
     , "dt".date as settlement_report
-    , fps.count as orders_count
-    , fps.total_sum as orders_total_sum
-    , fps.bonus_payment as orders_bonus_payment_sum
-    , fps.bonus_grant as orders_bonus_granted_sum
-    , fps.total_sum * 0.25 as order_processing_fee
-    , fps.count - fps.total_sum * 0.25 - fps.bonus_payment as restaurant_reward_sum
+    , COUNT(fps.count) as orders_count
+    , SUM(fps.total_sum) as orders_total_sum
+    , SUM(fps.bonus_payment) as orders_bonus_payment_sum
+    , SUM(fps.bonus_grant) as orders_bonus_granted_sum
+    , SUM(fps.total_sum) * 0.25 as order_processing_fee
+    , SUM(fps.total_sum) - SUM(fps.total_sum) * 0.25 - SUM(fps.bonus_payment) as restaurant_reward_sum
 FROM dds.fct_product_sales as fps
 JOIN dds.dm_orders as "do" on fps.order_id = "do".id 
 JOIN dds.dm_products as "dp" on fps.product_id = "dp".id
 JOIN dds.dm_restaurants as "dr" on "dp".restaurant_id = "dr".id
-JOIN dds.dm_timestamps as "dt" on "do".timestamp_id = "dt".id;
+JOIN dds.dm_timestamps as "dt" on "do".timestamp_id = "dt".id
+GROUP BY 1,2,3,4;
