9d6e0b6545053873ffe111cc44b9d066e27f2e5e
NikMaNik <nikolay.m96@mail.ru>
2025-04-11 02:48:47 +0500

    add

--- Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
+++ Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
@@ -1,3 +1,26 @@
+WITH aggregated_data AS (
+    SELECT
+        fps.id,
+        "dr".restaurant_id,
+        "dr".restaurant_name,
+        "dt".date::date as settlement_date,
+        SUM(fps.count) as orders_count,
+        SUM(fps.total_sum) as orders_total_sum,
+        SUM(fps.bonus_payment) as orders_bonus_payment_sum,
+        SUM(fps.bonus_grant) as orders_bonus_granted_sum,
+        SUM(fps.total_sum * 0.25) as order_processing_fee,
+        SUM(fps.total_sum - fps.total_sum * 0.25 - fps.bonus_payment) as restaurant_reward_sum
+    FROM dds.fct_product_sales as fps
+    JOIN dds.dm_orders as "do" on fps.order_id = "do".id 
+    JOIN dds.dm_products as "dp" on fps.product_id = "dp".id
+    JOIN dds.dm_restaurants as "dr" on "dp".restaurant_id = "dr".id
+    JOIN dds.dm_timestamps as "dt" on "do".timestamp_id = "dt".id
+    GROUP BY
+        fps.id,
+        "dr".restaurant_id,
+        "dr".restaurant_name,
+        "dt".date::date
+)
 INSERT INTO cdm.dm_settlement_report (
     id,
     restaurant_id,
@@ -10,22 +33,18 @@ INSERT INTO cdm.dm_settlement_report (
     order_processing_fee,
     restaurant_reward_sum
 )
-SELECT 
-    fps.id,
-    "do".restaurant_id,
-    "dr".restaurant_name,
-    "dt".date::date as settlement_date,
-    fps.count as orders_count,
-    fps.total_sum as orders_total_sum,
-    fps.bonus_payment as orders_bonus_payment_sum,
-    fps.bonus_grant as orders_bonus_granted_sum,
-    fps.total_sum * 0.25 as order_processing_fee,
-    fps.total_sum - fps.total_sum * 0.25 - fps.bonus_payment as restaurant_reward_sum
-FROM dds.fct_product_sales as fps
-JOIN dds.dm_orders as "do" on fps.order_id = "do".id 
-JOIN dds.dm_products as "dp" on fps.product_id = "dp".id
-JOIN dds.dm_restaurants as "dr" on "dp".restaurant_id = "dr".id
-JOIN dds.dm_timestamps as "dt" on "do".timestamp_id = "dt".id
+SELECT
+    id,
+    restaurant_id,
+    restaurant_name,
+    settlement_date,
+    orders_count,
+    orders_total_sum,
+    orders_bonus_payment_sum,
+    orders_bonus_granted_sum,
+    order_processing_fee,
+    restaurant_reward_sum
+FROM aggregated_data
 ON CONFLICT (restaurant_id, settlement_date) DO UPDATE SET
     orders_count = EXCLUDED.orders_count,
     orders_total_sum = EXCLUDED.orders_total_sum,
