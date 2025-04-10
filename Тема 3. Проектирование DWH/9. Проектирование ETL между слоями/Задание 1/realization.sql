dafbb3e7864ce3ec1c8e22da52177b0cb5a939f3
NikMaNik <nikolay.m96@mail.ru>
2025-04-11 03:06:23 +0500

    add

--- Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
+++ Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
@@ -14,6 +14,8 @@ WITH aggregated_data AS (
     JOIN dds.dm_products as "dp" on fps.product_id = "dp".id
     JOIN dds.dm_restaurants as "dr" on "dp".restaurant_id = "dr".id
     JOIN dds.dm_timestamps as "dt" on "do".timestamp_id = "dt".id
+    WHERE 
+        dds.dm_orders = 'closed'
     GROUP BY
         "dr".restaurant_id,
         "dr".restaurant_name,
