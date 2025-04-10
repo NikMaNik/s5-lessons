acbc5595e4c679e431980aa44423eb675e1e687d
NikMaNik <nikolay.m96@mail.ru>
2025-04-11 02:54:33 +0500

    add

--- Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
+++ Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
@@ -3,22 +3,19 @@ WITH aggregated_data AS (
         "dr".restaurant_id,
         "dr".restaurant_name,
         "dt".date::date as settlement_date,
-        SUM(fps.count) as orders_count,
-        SUM(fps.total_sum) as orders_total_sum,
-        SUM(fps.bonus_payment) as orders_bonus_payment_sum,
-        SUM(fps.bonus_grant) as orders_bonus_granted_sum,
-        SUM(fps.total_sum * 0.25) as order_processing_fee,
-        SUM(fps.total_sum - fps.total_sum * 0.25 - fps.bonus_payment) as restaurant_reward_sum
+        fps.count as orders_count,
+        fps.total_sum as orders_total_sum,
+        fps.bonus_payment as orders_bonus_payment_sum,
+        fps.bonus_grant as orders_bonus_granted_sum,
+        fps.total_sum * 0.25 as order_processing_fee,
+        fps.total_sum - fps.total_sum * 0.25 - fps.bonus_payment as restaurant_reward_sum
     FROM dds.fct_product_sales as fps
     JOIN dds.dm_orders as "do" on fps.order_id = "do".id 
     JOIN dds.dm_products as "dp" on fps.product_id = "dp".id
     JOIN dds.dm_restaurants as "dr" on "dp".restaurant_id = "dr".id
     JOIN dds.dm_timestamps as "dt" on "do".timestamp_id = "dt".id
-    GROUP BY
-        "dr".restaurant_id,
-        "dr".restaurant_name,
-        "dt".date::date
 )
+
 INSERT INTO cdm.dm_settlement_report (
     restaurant_id,
     restaurant_name,
