1cbe57af7cb8f5387a566a9178b601f68027abfe
NikMaNik <nikolay.m96@mail.ru>
2025-04-15 03:06:42 +0500

    add.

--- dags/examples/stg/bonus_system_ranks_dag/bonus_system_ranks_dag.py
+++ dags/examples/stg/bonus_system_ranks_dag/bonus_system_ranks_dag.py
@@ -36,7 +36,27 @@ def sprint5_example_stg_bonus_system_ranks_dag():
     # Т.к. таск один, просто обозначим его здесь.
     ranks_dict  # type: ignore
 
+def sprint5_example_stg_bonus_system_users_dag():
+    # Создаем подключение к базе dwh.
+    dwh_pg_connect = ConnectionBuilder.pg_conn("PG_WAREHOUSE_CONNECTION")
+
+    # Создаем подключение к базе подсистемы бонусов.
+    origin_pg_connect = ConnectionBuilder.pg_conn("PG_ORIGIN_BONUS_SYSTEM_CONNECTION")
+
+    # Объявляем таск, который загружает данные.
+    @task(task_id="users_load")
+    def load_users():
+        # создаем экземпляр класса, в котором реализована логика.
+        rest_loader = RankLoader(origin_pg_connect, dwh_pg_connect, log)
+        rest_loader.load_users()  # Вызываем функцию, которая перельет данные.
+
+    # Инициализируем объявленные таски.
+    ranks_dict = load_users()
+
+    # Далее задаем последовательность выполнения тасков.
+    # Т.к. таск один, просто обозначим его здесь.
+    ranks_dict  # type: ignore
 
 
 stg_bonus_system_ranks_dag = sprint5_example_stg_bonus_system_ranks_dag()
-
+stg_bonus_system_users_dag = sprint5_example_stg_bonus_system_users_dag()
