e3f03614642c41fc2a86035a0dc9b6f654665c06
NikMaNik <nikolay.m96@mail.ru>
2025-04-17 00:38:33 +0500

    add.

--- dags/examples/stg/bonus_system_ranks_dag/bonus_system_users_dag.py
+++ dags/examples/stg/bonus_system_ranks_dag/bonus_system_users_dag.py
@@ -15,7 +15,7 @@ log = logging.getLogger(__name__)
     tags=['sprint5', 'stg', 'origin', 'example'],  # Теги, используются для фильтрации в интерфейсе Airflow.
     is_paused_upon_creation=True  # Остановлен/запущен при появлении. Сразу запущен.
 )
-def sprint5_example_stg_bonus_system_ranks_dag():
+def sprint5_example_stg_bonus_system_users_dag():
     # Создаем подключение к базе dwh.
     dwh_pg_connect = ConnectionBuilder.pg_conn("PG_WAREHOUSE_CONNECTION")
 
@@ -23,18 +23,16 @@ def sprint5_example_stg_bonus_system_ranks_dag():
     origin_pg_connect = ConnectionBuilder.pg_conn("PG_ORIGIN_BONUS_SYSTEM_CONNECTION")
 
     # Объявляем таск, который загружает данные.
-    @task(task_id="ranks_load")
-    def load_ranks():
-        # создаем экземпляр класса, в котором реализована логика.
-        rest_loader = RankLoader(origin_pg_connect, dwh_pg_connect, log)
-        rest_loader.load_ranks()  # Вызываем функцию, которая перельет данные.
-
+    @task(task_id="users_load")
+    def load_users():
+        users_loader = UsersLoader(origin_pg_connect, dwh_pg_connect, log)
+        users_loader.load_users()
     # Инициализируем объявленные таски.
-    ranks_dict = load_ranks()
+    users_task = load_users()
     
     # Далее задаем последовательность выполнения тасков.
     # Т.к. таск один, просто обозначим его здесь.
-    ranks_dict# type: ignore
+    users_task# type: ignore
 
 
-stg_bonus_system_ranks_dag = sprint5_example_stg_bonus_system_ranks_dag()
+stg_bonus_system_ranks_dag = sprint5_example_stg_bonus_system_users_dag()
