d2bb245e8522d2230600a1eb6366a5d67fcca5dd
NikMaNik <nikolay.m96@mail.ru>
2025-04-17 13:36:22 +0500

    add.

--- dags/examples/stg/order_system_users_dag/users_reader.py
+++ dags/examples/stg/order_system_users_dag/users_reader.py
@@ -4,11 +4,11 @@ from typing import Dict, List
 from lib import MongoConnect
 
 
-class RestaurantReader:
+class UsersReader:
     def __init__(self, mc: MongoConnect) -> None:
         self.dbs = mc.client()
 
-    def get_restaurants(self, load_threshold: datetime, limit) -> List[Dict]:
+    def get_users(self, load_threshold: datetime, limit) -> List[Dict]:
         # Формируем фильтр: больше чем дата последней загрузки
         filter = {'update_ts': {'$gt': load_threshold}}
 
@@ -16,5 +16,5 @@ class RestaurantReader:
         sort = [('update_ts', 1)]
 
         # Вычитываем документы из MongoDB с применением фильтра и сортировки.
-        docs = list(self.dbs.get_collection("restaurants").find(filter=filter, sort=sort, limit=limit))
+        docs = list(self.dbs.get_collection("users").find(filter=filter, sort=sort, limit=limit))
         return docs
