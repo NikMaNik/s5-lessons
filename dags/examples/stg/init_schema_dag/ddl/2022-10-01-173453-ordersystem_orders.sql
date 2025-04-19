b8353668294832f1b859443073d8518ff453d4be
NikMaNik <nikolay.m96@mail.ru>
2025-04-19 17:17:31 +0500

    add.

--- dags/examples/stg/init_schema_dag/ddl/2022-10-01-173453-ordersystem_orders.sql
+++ dags/examples/stg/init_schema_dag/ddl/2022-10-01-173453-ordersystem_orders.sql
@@ -2,6 +2,6 @@ create table if not exists stg.ordersystem_orders
 (
     id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     object_id varchar not null UNIQUE,
-    object_values text not null,
+    object_value text not null,
     update_ts timestamp not null
 )
