846f214d9d0ef48b5784d144e7e4cf29e96fb13c
NikMaNik <nikolay.m96@mail.ru>
2025-04-19 18:55:45 +0500

    add.

--- dags/examples/dds/dm_users/dm_users_loader.py
+++ dags/examples/dds/dm_users/dm_users_loader.py
@@ -61,7 +61,7 @@ class UserDestRepository:
 class UserLoader:
     WF_KEY = "example_users_origin_to_stg_workflow"
     LAST_LOADED_ID_KEY = "last_loaded_id"
-    BATCH_LIMIT = 1  # Рангов мало, но мы хотим продемонстрировать инкрементальную загрузку рангов.
+    BATCH_LIMIT = 100  # Рангов мало, но мы хотим продемонстрировать инкрементальную загрузку рангов.
 
     def __init__(self, pg_origin: PgConnect, pg_dest: PgConnect, log: Logger) -> None:
         self.pg_dest = pg_dest
