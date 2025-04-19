15b219b3bd70020628b01e503778609baa259fbf
NikMaNik <nikolay.m96@mail.ru>
2025-04-19 22:11:35 +0500

    "add."

---
+++ dags/examples/dds/dm_timestamps/dm_restaurants_dag.py
@@ -0,0 +1,39 @@
+import logging
+
+import pendulum
+from airflow.decorators import dag, task
+from examples.dds.dm_restaurants.dm_restaurants_loader import RestaurantLoader
+from lib import ConnectionBuilder
+
+log = logging.getLogger(__name__)
+
+
+@dag(
+    schedule_interval='0/15 * * * *',  # Задаем расписание выполнения дага - каждый 15 минут.
+    start_date=pendulum.datetime(2022, 5, 5, tz="UTC"),  # Дата начала выполнения дага. Можно поставить сегодня.
+    catchup=False,  # Нужно ли запускать даг за предыдущие периоды (с start_date до сегодня) - False (не нужно).
+    tags=['sprint5', 'dds',  'example'],  # Теги, используются для фильтрации в интерфейсе Airflow.
+    is_paused_upon_creation=True  # Остановлен/запущен при появлении. Сразу запущен.
+)
+
+def sprint5_example_dds_restaurants_dag():
+    # Создаем подключение к базе dwh.
+    dwh_pg_connect = ConnectionBuilder.pg_conn("PG_WAREHOUSE_CONNECTION")
+
+    # # Создаем подключение к базе подсистемы бонусов.
+    # origin_pg_connect = ConnectionBuilder.pg_conn("PG_ORIGIN_BONUS_SYSTEM_CONNECTION")
+
+    # Объявляем таск, который загружает данные.
+    @task(task_id="restaurants_load")
+    def load_restaurants():
+        events_loader = RestaurantLoader(dwh_pg_connect, dwh_pg_connect, log)
+        events_loader.load_restaurant()
+
+    # Подключаем задачу загрузки событий в цепочку зависимостей
+    dm_restaurants_task = load_restaurants()
+
+    # Теперь порядок выполнения операций следующий:
+    dm_restaurants_task
+    
+
+stg_bonus_system_ranks_dag = sprint5_example_dds_restaurants_dag()
