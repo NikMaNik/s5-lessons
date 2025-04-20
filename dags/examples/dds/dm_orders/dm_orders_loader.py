f5a7ac6d52c56da32aa48411e4c6ef68d9e1f269
NikMaNik <nikolay.m96@mail.ru>
2025-04-20 22:30:23 +0500

    add

--- dags/examples/dds/dm_orders/dm_orders_loader.py
+++ dags/examples/dds/dm_orders/dm_orders_loader.py
@@ -155,7 +155,7 @@ class OrderDestRepository:
 class OrderLoader:
     WF_KEY = "example_dm_order_workflow"
     LAST_LOADED_ID_KEY = "last_loaded_id"
-    BATCH_LIMIT = 29  # Рангов мало, но мы хотим продемонстрировать инкрементальную загрузку рангов.
+    BATCH_LIMIT = 1000  # Рангов мало, но мы хотим продемонстрировать инкрементальную загрузку рангов.
 
     def __init__(self, pg_origin: PgConnect, pg_dest: PgConnect, log: Logger) -> None:
         self.pg_dest = pg_dest
