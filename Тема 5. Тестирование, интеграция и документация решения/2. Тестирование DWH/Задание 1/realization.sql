5ebb14cd16c304b0929b5cae4edd17a88175aec9
NikMaNik <nikolay.m96@mail.ru>
2025-04-21 12:59:53 +0500

    add

--- Тема 5. Тестирование, интеграция и документация решения/2. Тестирование DWH/Задание 1/realization.sql
+++ Тема 5. Тестирование, интеграция и документация решения/2. Тестирование DWH/Задание 1/realization.sql
@@ -1,1 +1,19 @@
-select test_date_time, test_name, test_result from public_test.testing_result;
+SELECT 
+    current_timestamp at  time zone 'UTC' AS test_date_time,
+    'test_01' AS test_name,
+    CASE
+        WHEN COUNT(*) > 0 THEN True  -- Если есть хотя бы одно расхождение, тест неуспешен
+        ELSE False                   -- Иначе тест успешен
+    END AS test_result
+FROM 
+    public_test.dm_settlement_report_actual a
+FULL OUTER JOIN 
+    public_test.dm_settlement_report_expected e 
+ON
+    a.restaurant_id = e.restaurant_id AND
+    a.settlement_year = e.settlement_year AND
+    a.settlement_month = e.settlement_month
+WHERE
+    a.id IS NULL OR e.id IS NULL;
+
+select * from public_test.testing_result;
