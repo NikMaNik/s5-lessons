b3920bc7a00d3b02cd067ebb6da77b9a76ce70ad
NikMaNik <nikolay.m96@mail.ru>
2025-04-21 12:51:43 +0500

    add

---
+++ Тема 5. Тестирование, интеграция и документация решения/2. Тестирование DWH/Задание 1/realization.sql
@@ -0,0 +1,23 @@
+INSERT INTO public_test.testing_result
+(
+    test_date_time,
+    test_name,
+    test_result
+)
+SELECT 
+    CURRENT_TIMESTAMP AS test_date_time,
+    'test_01' AS test_name,
+    CASE
+        WHEN COUNT(*) > 0 THEN True  -- Если есть хотя бы одно расхождение, тест неуспешен
+        ELSE False                   -- Иначе тест успешен
+    END AS test_result
+FROM 
+    public_test.dm_settlement_report_actual a
+FULL OUTER JOIN 
+    public_test.dm_settlement_report_expected e 
+ON
+    a.restaurant_id = e.restaurant_id AND
+    a.settlement_year = e.settlement_year AND
+    a.settlement_month = e.settlement_month
+WHERE
+    a.id IS NULL OR e.id IS NULL;
